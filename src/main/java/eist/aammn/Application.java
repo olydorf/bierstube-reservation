5f1adfc99507b8cc0c9acd0a49b02271eebf4401
ismail ben ayed <ismailbenayed5@gmail.com>
2023-10-19 17:46:17 +0200

    added SQL script to reset the database (reservations and restaurant_tables)

--- src/main/java/eist/aammn/Application.java
+++ src/main/java/eist/aammn/Application.java
@@ -2,10 +2,12 @@ package eist.aammn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableScheduling
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
